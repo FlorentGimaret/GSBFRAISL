b4195633a1f6d9091d2085df95283c8b17afc6df
MROLLET <mireille.rollet@ac-lyon.fr>
2017-12-04 13:41:00 +0100

    initialisation du projet

---
+++ app/Http/Controllers/ConnexionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\metier\GsbFrais;
+class ConnexionController extends Controller
+{
+    /**
+     * Authentifie le visiteur
+     * @return type Vue formLogin ou home
+     */
+    public function logIn(Request $request) {
+        $login = $request->input('login');
+        $pwd = $request->input('pwd'); 
+        $gsbFrais = new GsbFrais();
+        $res = $gsbFrais->getInfosVisiteur($login,$pwd);
+        if(empty($res)){
+            Session::put('id', '0');
+            $erreur = "Login ou mot de passe inconnu !";
+            return view('formLogin', compact('erreur'));
+        }
+        else{
+            $visiteur = $res[0];
+            $id = $visiteur->id;
+            $nom =  $visiteur->nom;
+            $prenom = $visiteur->prenom;
+            Session::put('id', $id);
+            Session::put('nom', $nom);
+            Session::put('prenom', $prenom);
+//            return view('home');
+            return redirect('/');
+        }
+    }
+    
+    /**
+     * Déconnecte le visiteur authentifié
+     * @return type Vue home
+     */
+    public function logOut(){
+        Session::put('id', '0');
+//        Session::forget('id');
+        return view('home');
+    }
+    
+    /**
+     * Initialise le formulaire d'authentification
+     * @return type Vue formLogin
+     */
+    public function getLogin() {
+        $erreur = "";
+        return view ('formLogin', compact('erreur'));
+    }
+}
